030f9034898ef9a0bbfa71a888dbf7ede0f3effa
foreverchild <harrisonogbonnaya@gmail.com>
2022-02-24 14:28:12 +0100

    folder structure init

---
+++ movieRepoBackend/src/main/java/com/groupone/movierepobackend/data/models/Playlist.java
@@ -0,0 +1,27 @@
+package com.groupone.movierepobackend.data.models;
+
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Getter
+@Setter
+public class Playlist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @ManyToOne
+    private UserAccount user;
+    private String name;
+    private String description;
+
+}
