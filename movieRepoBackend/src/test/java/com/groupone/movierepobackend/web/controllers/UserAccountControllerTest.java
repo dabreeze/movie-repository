39ccc661e27b2b08498d2686957a812a28c8a0db
foreverchild <harrisonogbonnaya@gmail.com>
2022-02-24 12:20:31 +0100

    folder structure init

---
+++ movieRepoBackend/src/test/java/com/groupone/movierepobackend/web/controllers/UserAccountControllerTest.java
@@ -0,0 +1,12 @@
+package com.groupone.movierepobackend.web.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserAccountControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
