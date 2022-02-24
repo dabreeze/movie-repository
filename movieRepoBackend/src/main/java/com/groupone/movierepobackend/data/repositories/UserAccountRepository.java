b5ed6864f827eaf57f7157c43e762255183db027
foreverchild <harrisonogbonnaya@gmail.com>
2022-02-24 14:24:56 +0100

    folder structure init

--- movieRepoBackend/src/main/java/com/groupone/movierepobackend/data/repositories/UserAccountRepository.java
+++ movieRepoBackend/src/main/java/com/groupone/movierepobackend/data/repositories/UserAccountRepository.java
@@ -3,5 +3,8 @@ package com.groupone.movierepobackend.data.repositories;
 import com.groupone.movierepobackend.data.models.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+//    Optional <UserAccount> findByEmail(String email);
 }
