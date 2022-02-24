00676a761beb4e7d20dfe474960ce35e25ee7e2c
foreverchild <harrisonogbonnaya@gmail.com>
2022-02-24 12:20:18 +0100

    folder structure init

---
+++ movieRepoBackend/src/main/java/com/groupone/movierepobackend/web/exceptions/MovieRepositoryException.java
@@ -0,0 +1,7 @@
+package com.groupone.movierepobackend.web.exceptions;
+
+public class MovieRepositoryException extends Throwable {
+    public MovieRepositoryException(String message) {
+        super(message);
+    }
+}
