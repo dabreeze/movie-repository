f947bdc181edadcda441ba32d7175e377247c292
foreverchild <harrisonogbonnaya@gmail.com>
2022-02-24 14:25:07 +0100

    folder structure init

--- movieRepoBackend/src/main/java/com/groupone/movierepobackend/data/models/UserAccount.java
+++ movieRepoBackend/src/main/java/com/groupone/movierepobackend/data/models/UserAccount.java
@@ -1,10 +1,30 @@
 package com.groupone.movierepobackend.data.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
+@Data
 public class UserAccount {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    private String firstName;
+    private String lastName;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany
+    private List<Playlist> playlists;
+    @Column(unique = true, nullable = false)
+    private String email;
+    @CreationTimestamp
+    private LocalDateTime dateCreated;
+
+
 }
