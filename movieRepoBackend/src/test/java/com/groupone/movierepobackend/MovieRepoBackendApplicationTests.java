3ac580bf1a2602d34c3c62f04496cec5a7a42569
foreverchild <harrisonogbonnaya@gmail.com>
2022-02-24 13:13:42 +0100

    folder structure init

---
+++ movieRepoBackend/src/test/java/com/groupone/movierepobackend/MovieRepoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupone.movierepobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRepoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
