556a8c4392f3bb1e0e22567e391573443054e936
foreverchild <harrisonogbonnaya@gmail.com>
2022-02-24 12:20:23 +0100

    folder structure init

---
+++ movieRepoBackend/src/main/java/com/groupone/movierepobackend/data/models/UserAccount.java
@@ -0,0 +1,10 @@
+package com.groupone.movierepobackend.data.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class UserAccount {
+    @Id
+    private Long id;
+}
