4a52b31ceb39582d41fe6229ecb366eecbaa9f3e
foreverchild <harrisonogbonnaya@gmail.com>
2022-02-24 13:13:16 +0100

    folder structure init

---
+++ movieRepoBackend/src/main/java/com/groupone/movierepobackend/MovieRepoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.groupone.movierepobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRepoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieRepoBackendApplication.class, args);
+    }
+
+}
