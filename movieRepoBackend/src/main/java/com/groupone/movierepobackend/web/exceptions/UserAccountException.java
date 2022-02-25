317c8ac411862ca632d2753c0df674e62fead588
Gideon Udoh <geediegramgu@gmail.com>
2022-02-25 08:05:57 +0100

    user account

---
+++ movieRepoBackend/src/main/java/com/groupone/movierepobackend/web/exceptions/UserAccountException.java
@@ -0,0 +1,7 @@
+package com.groupone.movierepobackend.web.exceptions;
+
+public class UserAccountException extends Throwable {
+    public UserAccountException(String message) {
+        super(message);
+    }
+}
