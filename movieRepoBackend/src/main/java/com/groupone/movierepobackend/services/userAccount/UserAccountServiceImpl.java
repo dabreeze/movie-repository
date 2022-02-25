d136c734cdc3e59842bca74e27d98da6de28eb9c
Gideon Udoh <geediegramgu@gmail.com>
2022-02-25 07:52:14 +0100

    user account service implementation

---
+++ movieRepoBackend/src/main/java/com/groupone/movierepobackend/services/userAccount/UserAccountServiceImpl.java
@@ -0,0 +1,47 @@
+package com.groupone.movierepobackend.services.userAccount;
+
+import com.groupone.movierepobackend.data.dtos.UserAccountRequestDto;
+import com.groupone.movierepobackend.data.dtos.UserAccountResponseDto;
+import com.groupone.movierepobackend.data.models.UserAccount;
+import com.groupone.movierepobackend.data.repositories.UserAccountRepository;
+import com.groupone.movierepobackend.web.exceptions.UserAccountException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserAccountServiceImpl implements UserAccountService{
+
+    @Autowired
+    UserAccountRepository userAccountRepository;
+
+    @Override
+    public UserAccountResponseDto createMovieUser(UserAccountRequestDto requestDto) throws UserAccountException {
+        //check that user exists
+        Optional<UserAccount> savedUserAccount = userAccountRepository.findByEmail(requestDto.getEmail());
+        if (savedUserAccount.isPresent()){
+            throw new UserAccountException("This User already exists");
+        }
+
+        //create a movie user
+        UserAccount newUser = new UserAccount();
+        newUser.setFirstName(requestDto.getFirstName());
+        newUser.setLastName(requestDto.getLastName());
+        newUser.setEmail(requestDto.getEmail());
+
+        //save newly created user
+        userAccountRepository.save(newUser);
+
+        //return response
+        return buildResponse(newUser);
+    }
+
+    private UserAccountResponseDto buildResponse(UserAccount userAccount){
+        return UserAccountResponseDto.builder()
+                .firstName(userAccount.getFirstName())
+                .lastName(userAccount.getLastName())
+                .email(userAccount.getEmail())
+                .build();
+    }
+}
