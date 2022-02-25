d75dbc515a837ad68b2fec7552cfd5e492ace9a7
Gideon Udoh <geediegramgu@gmail.com>
2022-02-25 08:01:54 +0100

    user account

--- movieRepoBackend/src/main/java/com/groupone/movierepobackend/data/repositories/UserAccountRepository.java
+++ movieRepoBackend/src/main/java/com/groupone/movierepobackend/data/repositories/UserAccountRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
-//    Optional <UserAccount> findByEmail(String email);
+    Optional <UserAccount> findByEmail(String email);
 }
