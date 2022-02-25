2938f97edc0256a80fc52dbd7f23e7b78be51eab
olatoye <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>
2022-02-25 10:05:47 +0100

    movie

--- movieRepoBackend/src/main/java/com/groupone/movierepobackend/data/models/Movie.java
+++ movieRepoBackend/src/main/java/com/groupone/movierepobackend/data/models/Movie.java
@@ -1,27 +1,38 @@
 package com.groupone.movierepobackend.data.models;
 
-import lombok.Getter;
-import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
 
-import javax.persistence.Id;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Column;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class Movie {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonProperty("id")
     private Long id;
 
-    @Column(unique = true, nullable = false)
-    private String movieName;
+    @Column(nullable = false)
+    @JsonProperty("title")
+    private String title;
+
+    @JsonProperty("description")
     private String description;
-    private Double rating;
-    private String url;
+
+    @JsonProperty("imDbRating")
+    private Double imDbRating;
+
+    @JsonProperty("image")
+    private String image;
+
+    @ElementCollection
+    private List<MovieGenre> genres;
 }
