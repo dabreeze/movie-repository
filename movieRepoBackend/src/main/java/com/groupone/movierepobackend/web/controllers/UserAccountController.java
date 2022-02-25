f66e1e397c7f1c15c313e08a644c438ad14f2d90
Gideon Udoh <geediegramgu@gmail.com>
2022-02-25 08:01:41 +0100

    user account

--- movieRepoBackend/src/main/java/com/groupone/movierepobackend/web/controllers/UserAccountController.java
+++ movieRepoBackend/src/main/java/com/groupone/movierepobackend/web/controllers/UserAccountController.java
@@ -1,4 +1,30 @@
 package com.groupone.movierepobackend.web.controllers;
 
+
+import com.groupone.movierepobackend.data.dtos.UserAccountRequestDto;
+import com.groupone.movierepobackend.data.dtos.UserAccountResponseDto;
+import com.groupone.movierepobackend.services.userAccount.UserAccountService;
+import com.groupone.movierepobackend.web.exceptions.UserAccountException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/account")
 public class UserAccountController {
+
+    @Autowired
+    UserAccountService userAccountService;
+
+    public ResponseEntity<?> createUserAccount(@RequestBody UserAccountRequestDto userAccountRequestDto){
+        try {
+            UserAccountResponseDto responseDto = userAccountService.createMovieUser(userAccountRequestDto);
+            return ResponseEntity.ok().body(responseDto);
+        } catch (UserAccountException ex){
+            return ResponseEntity.badRequest().body(ex);
+        }
+
+    }
 }
